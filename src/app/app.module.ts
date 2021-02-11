0a9fe510482a04d23c6b049866f1398804a7d484
fabius81 <fabius219@hotmail.it>
2021-02-11 22:03:26 +0100

    CanActivate
    CanActivate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,24 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
 import { AboutComponent } from './about/about.component';
 import { SspsComponent } from './ssps/ssps.component';
+import { UsersComponent } from './users/users.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 const appRoutes: Routes = [
   {
-    path: 'dashboard', component: HeaderComponent, 
+    path: 'dashboard', canActivate:[AuthGuard], component: HeaderComponent, 
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'about', component: AboutComponent },
+      { path: 'reports', component: ReportsComponent },
+      { path: 'users', component: UsersComponent },      
+      { path: 'ssps', component: SspsComponent },
+      { path: 'contacts', component: ContactsComponent }
     ]
   },
   { path: 'login', component: LoginComponent },
@@ -28,13 +37,16 @@ const appRoutes: Routes = [
     HeaderComponent,
     HomeComponent,
     AboutComponent,
-    SspsComponent
+    ReportsComponent,
+    SspsComponent,
+    UsersComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
